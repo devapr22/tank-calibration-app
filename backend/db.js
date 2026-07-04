dda0b95b89afceee0f31088f8f7ae1704c21a4be
devapr22 <devapr99@gmail.com>
2026-07-04 12:43:37 +0530

    prepare backend for deployment

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,22 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "tank_calibration",
-    password: "root1",
-    port: 5432,
-});
+const { Pool } = require("pg");
+
+const pool = process.env.DATABASE_URL
+    ? new Pool({
+          connectionString: process.env.DATABASE_URL,
+          ssl: {
+              rejectUnauthorized: false,
+          },
+      })
+    : new Pool({
+          user: "postgres",
+          host: "localhost",
+          database: "tank_calibration",
+          password: "root1",
+          port: 5432,
+      });
+
+module.exports = pool;
 
 module.exports = pool;
